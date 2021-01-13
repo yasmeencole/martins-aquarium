0b37d2c385467281da2736961048687e1ca6cd0d
yasmeencole <yasmeencole1@mail.com>
2021-01-13 13:25:31 -0600

    made updates to html, css, and js

---
+++ scripts/Fish.js
@@ -0,0 +1,17 @@
+/**
+ *  Fish which renders individual fish objects as HTML
+ */
+export const Fish = (fish) => {
+    return `
+        <section class="fishCardBox">
+            <div class="fishCard">
+                <div><img  class="fish__image image--card" src="${fish.image}" /></div>
+                <div class="fish__name">${fish.name}</div>
+                <div class="fish__species">${fish.species}</div>
+                <div class="fish__length">${fish.length}</div>
+                <div class="fish__location">${fish.location}</div>
+                <div class="fish__diet">${fish.food}</div>
+            </div>    
+        </section>
+    `
+}
